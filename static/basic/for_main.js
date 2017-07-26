ffaff39f77abc917596ce04ae428eeaeeb97ea13
minaev007 <yurij.zusik@gmail.com>
2017-07-26 17:56:58 +0300

    static collected

--- static/basic/for_main.js
+++ static/basic/for_main.js
@@ -1,6 +1,6 @@
 function main() {
     checking = false;
-
+    console.log('HELLO GIT!');
     /* slider */
 
     var slideWidth=995;
@@ -128,7 +128,6 @@ function main() {
                     $('.my-slide img').removeClass('my-slide-img-small');                    
                     $('.my-slide img').addClass('my-slide-img-big');                                    
                 }
-                console.log(slideWidth);
             },
             mql = window.matchMedia('all and (max-width: 1020px)');
         handleMatchMedia(mql);
